c5e6372df83b73e718ec0a984c703034df95ef7b
kotyaTOP <katerinaj@bk.ru>
2019-06-04 00:11:56 +0300

    Все еще пытаюсь исправить то что натворила

--- Project/service.py
+++ Project/service.py
@@ -8,7 +8,29 @@ from Project.forms import DishForm, IngredForm, UnitForm, DishIngredForm, FormSe
 def menu():
     return render_template('./menu.html')
 
+@app.route('/change_dish_ingred/<id>/<do>', methods=['GET', 'POST'])
+def change_dish_ingred(id, do):
+    id = int(id)
 
+    if do == "add":
+        s = Dish_Ingredient()
+    else:
+        s = mydb.session.query(Dish_Ingredient).filter(Dish_Ingredient.id_dish_ingred == id).one_or_none()
+
+    form = DishIngredForm(request.form, obj=s)
+
+    if do == "delete":
+        mydb.session.delete(s)
+        mydb.session.flush()
+        return redirect(url_for('dish_ingred'))
+
+    if form.button_save.data:
+        form.populate_obj(s)
+        mydb.session.add(s)
+        if s.id_dish_ingred != id:
+            return redirect(url_for('dish_ingred', id=s.id_dish_ingred))
+
+    return render_template('change_dish_ingred.html', form=form)
 
 if __name__ == '__main__':
     metadata = mydb.metadata
